da8db8a20d68a56b5497627711a20041844b70e9
Miguel <guzmi592@gmail.com>
2024-07-11 15:59:42 +0200

    Actualizado README

--- src/services/marvel.ts
+++ src/services/marvel.ts
@@ -14,7 +14,7 @@ export const getFilms = async () => {
   return films;
 };
 
-export const getFilmBy = async ({id} : {id: string}) => {
+export const getFilmBy = async ({ id }: { id: string }) => {
   const response = await fetch(`${api_casera}film/${id}`, {
     headers: { Authorization: `Bearer ${token_api_casera}` },
   });
@@ -24,22 +24,27 @@ export const getFilmBy = async ({id} : {id: string}) => {
 };
 
 export const getCharacters = async (amount) => {
-  const response = await fetch(`${api_marvel}characters?series=22547&limit=${amount}&ts=${ts}&apikey=${token_marvel}&hash=${hash}`);
+  const response = await fetch(
+    `${api_marvel}characters?series=22547&limit=${amount}&ts=${ts}&apikey=${token_marvel}&hash=${hash}`
+  );
 
   const characters = await response.json();
   return characters.data.results;
 };
 
-export const getCharacterBy = async ({id} : {id: string}) => {
-  const response = await fetch(`${api_marvel}characters/${id}?ts=1&apikey=bd83974f8c0124c0b4006bb69fecc3b4&hash=6253d203f94171a77b3b7f026a16e3b7`);
+export const getCharacterBy = async ({ id }: { id: string }) => {
+  const response = await fetch(
+    `${api_marvel}characters/${id}?ts=1&apikey=bd83974f8c0124c0b4006bb69fecc3b4&hash=6253d203f94171a77b3b7f026a16e3b7`
+  );
   const character = await response.json();
   return character.data.results[0];
 };
 
 export const getComics = async () => {
-  const response = await fetch(`${api_marvel}comics?limit=48&ts=${ts}&apikey=${token_marvel}&hash=${hash}`);
+  const response = await fetch(
+    `${api_marvel}comics?limit=48&ts=${ts}&apikey=${token_marvel}&hash=${hash}`
+  );
 
   const comics = await response.json();
   return comics.data.results;
 };
-
